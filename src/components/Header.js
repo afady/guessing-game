6ed426931b3bee40b549554cc24786e0b1451b1b
Fady Farid <fadyfarid55@gmail.com>
2018-08-10 00:19:44 -0400

    update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,7 @@ export default () => {
   return (
     <nav className="light-blue lighten-1" role="navigation">
       <div className="nav-wrapper container">
-        <a id="logo-container" href="#" className="brand-logo">
-          Logo
-        </a>
-        <ul className="right hide-on-med-and-down">
-          <li>
-            <a href="#">Navbar Link</a>
-          </li>
-        </ul>
-
-        <ul id="nav-mobile" className="sidenav">
-          <li>
-            <a href="#">Navbar Link</a>
-          </li>
-        </ul>
-        <a href="#" data-target="nav-mobile" className="sidenav-trigger">
-          <i className="material-icons">menu</i>
-        </a>
+        <span className="brand-logo">Guessing Game</span>
       </div>
     </nav>
   );
